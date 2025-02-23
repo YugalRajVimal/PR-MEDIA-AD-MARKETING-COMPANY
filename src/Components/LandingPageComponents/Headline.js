fcb75bd7bde39660d76a811e7cc61935540b5739
YugalRajVimal <sagaryugal472@gmail.com>
2025-02-23 17:25:38 +0530

    --

--- src/Components/LandingPageComponents/Headline.js
+++ src/Components/LandingPageComponents/Headline.js
@@ -19,7 +19,7 @@ const Headline = () => {
   return (
     <div className="relative w-full overflow-hidden py-4 px-2 text-center">
       <div ref={marqueeRef} className="inline-block text-black w-full">
-        <span className="text-xl md:text-3xl lg:text-4xl font-bold">
+        <span className="text-2xl md:text-3xl lg:text-4xl font-bold">
           The PR Media - Luxury PR & Marketing Company
         </span>
       </div>
