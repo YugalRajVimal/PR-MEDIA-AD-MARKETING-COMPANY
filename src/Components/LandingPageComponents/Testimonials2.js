eafe1147cf2e40dcb42ac3e101124a83928e73e4
YugalRajVimal <sagaryugal472@gmail.com>
2025-02-26 23:35:09 +0530

    --

--- src/Components/LandingPageComponents/Testimonials2.js
+++ src/Components/LandingPageComponents/Testimonials2.js
@@ -2,11 +2,14 @@ import React, { useEffect, useRef } from "react";
 
 const Testimonials2 = () => {
   return (
-    <div id="testimonials" className="p-4 sm:p-10 md:p-20 text-[#fff2e1]">
+    <div id="testimonials" className="py-4 sm:py-10 md:py-20  text-[#fff2e1] ">
       <h2 className="text-4xl md:text-5xl lg:text-6xl text-center text-[#3B2E22] font-semibold p-6">
         Testimonials
       </h2>
-      <div className="flex flex-col items-center gap-2 max-w-3xl mx-auto bg-[#3B2E22] rounded-xl">
+      <div className="relative flex flex-col items-center gap-2 w-full h-[888px]  mx-auto bg-[#3B2E22] z-[-2]">
+        <div className="h-[106%] w-full absolute -top-[3%] left-0 z-[-1]">
+          <img src="bg1.png" className="h-full w-full" />
+        </div>
         {[
           { name: "Iman Gadzhi", role: "Founder", img: "user.png" },
           {
@@ -15,10 +18,11 @@ const Testimonials2 = () => {
             img: "user.png",
           },
           { name: "Ciaran Anderson", role: "Copywriter", img: "user.png" },
+          { name: "Ciaran Anderson", role: "Copywriter", img: "user.png" },
         ].map((person, index) => (
           <div
             key={index}
-            className="flex flex-col items-center gap-2 p-6 rounded-xl w-full"
+            className="flex flex-col items-center gap-2 p-6 rounded-xl w-full z-[1]"
           >
             <div className="h-24 w-24 rounded-full border-4 border-[#9D2140] overflow-hidden">
               <img
