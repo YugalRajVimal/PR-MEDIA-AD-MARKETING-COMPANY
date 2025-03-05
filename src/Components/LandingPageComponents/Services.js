13c8be9abeb5a87fbe5a3a9482639d559452c298
YugalRajVimal <sagaryugal472@gmail.com>
2025-03-06 00:40:35 +0530

    --

--- src/Components/LandingPageComponents/Services.js
+++ src/Components/LandingPageComponents/Services.js
@@ -43,15 +43,15 @@ const ServiceCard = ({ title, items }) => {
       <h2 className="text-xl font-bold text-center pt-3">{title}</h2>
       <div className="w-[90%] md:w-[70%] lg:w-[60%] border border-black mx-auto my-4"></div>
 
-      <ul className="sticky top-[15%] mt-4 space-y-4 relative text-md md:text-base">
+      <ul className="sticky top-[15%] mt-4 space-y-2 relative text-md md:text-base">
         {items.map((item, index) => (
           <li
             key={index}
-            className="sticky top-[15%] flex justify-center relative h-48"
+            className="sticky top-[15%] flex justify-center relative h-52"
           >
             <div className="sticky top-[15%] h-52 w-full max-w-[500px] p-2">
-              <div className="relative h-full text-black font-bold w-full border border-zinc-700 bg-[#dbc3ab] border-[2px] rounded-xl p-4  md:p-6">
-                <div className="flex flex-col gap-4">
+              <div className="relative h-full text-black flex justify-center items-center font-bold w-full border border-zinc-700 bg-[#dbc3ab] border-[2px] rounded-xl p-4  md:p-6">
+                <div className="flex flex-col gap-4 ">
                   <span>
                     <img
                       src={item.image}
@@ -73,7 +73,7 @@ const ServiceCard = ({ title, items }) => {
 
 const BusinessSalesSection = () => {
   return (
-    <div id="services" className="p-4 sm:p-6 md:p-10 min-h-[200vh]">
+    <div id="services" className=" md:p-10 min-h-[200vh]">
       <div className="flex flex-col items-center space-y-10  p-2 md:p-8 rounded-xl">
         {services.map((service, index) => (
           <ServiceCard key={index} {...service} />
