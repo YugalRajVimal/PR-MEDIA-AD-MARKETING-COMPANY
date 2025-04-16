b947589b15faf2cb5c73d9e594a709e6ac21f13c
YugalRajVimal <sagaryugal472@gmail.com>
2025-04-16 23:22:29 +0530

    --

--- src/Components/LandingPageComponents/VideoCard1.js
+++ src/Components/LandingPageComponents/VideoCard1.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const VideoCard1 = () => {
   return (
-    <div className="flex  items-center px-6  text-center">
+    <div className="flex justify-center items-center px-6  text-center">
       <div className="grid grid-cols-2 gap-6 mt-6 w-full max-w-2xl text-white">
         {/* First Video Card */}
         <div className="bg-[#3B2E22] py-1 px-1 rounded-xl shadow-lg border border-yellow-500 hover:shadow-yellow-400 transition-all flex flex-col justify-center items-center">
           <video
             controls
-            className="w-full rounded-lg"
+            className="w-full h-full rounded-lg"
             src="/video00001.mp4"
             poster="/video1Thumbnail.jpg" // 👈 Add your poster image path
           >
@@ -20,7 +20,7 @@ const VideoCard1 = () => {
         <div className="bg-[#3B2E22] py-1 px-1 rounded-xl shadow-lg border border-yellow-500 hover:shadow-yellow-400 transition-all flex flex-col justify-center items-center">
           <video
             controls
-            className="w-full rounded-lg"
+            className="w-full h-full rounded-lg"
             src="/video00002.mp4"
             poster="/video2Thumbnail.jpg" // 👈 Add your poster image path
           >
