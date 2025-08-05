28400506415007f2c1ac717e13edca8139a32881
YugalRajVimal <sagaryugal472@gmail.com>
2025-08-05 12:52:55 +0530

    --

--- src/Components/MessageBox/MessageBox.js
+++ src/Components/MessageBox/MessageBox.js
@@ -12,14 +12,14 @@ const getRangeForCurrentTime = () => {
   const now = new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" });
   const hour = new Date(now).getHours();
 
-  if (hour === 1) return [60000, 80000];
-  if (hour === 2) return [30000, 60000];
-  if (hour === 3) return [20000, 30000];
-  if (hour === 4) return [10000, 20000];
-  if (hour === 5) return [5000, 10000];
-  if (hour === 6) return [500, 5000];
-  if (hour === 7) return [500, 10000];
-  if (hour === 8) return [10000, 30000];
+  if (hour === 1) return [5000, 7000];
+  if (hour === 2) return [2000, 3000];
+  if (hour === 3) return [500, 1000];
+  if (hour === 4) return [500, 1000];
+  if (hour === 5) return [500, 1000];
+  if (hour === 6) return [1000, 2000];
+  if (hour === 7) return [4000, 6000];
+  if (hour === 8) return [8000, 12000];
   if (hour === 9) return [30000, 50000];
   if (hour === 10) return [50000, 70000];
   if (hour === 11) return [70000, 90000];
@@ -32,10 +32,10 @@ const getRangeForCurrentTime = () => {
   if (hour === 18) return [200000, 220000];
   if (hour === 19) return [200000, 220000];
   if (hour === 20) return [180000, 200000];
-  if (hour === 21) return [150000, 180000];
-  if (hour === 22) return [120000, 150000];
-  if (hour === 23) return [100000, 120000];
-  return [80000, 100000]; // 11PM to 1AM
+  if (hour === 21) return [70000, 80000];
+  if (hour === 22) return [50000, 60000];
+  if (hour === 23) return [20000, 10000];
+  return [10000, 12000];
 };
 
 const MessageBox = ({ setIsCustomerLoginVisible }) => {
@@ -164,7 +164,7 @@ const MessageBox = ({ setIsCustomerLoginVisible }) => {
     };
 
     updateCount(); // Initial update
-    const interval = setInterval(updateCount, 5 * 60 * 1000); // Every 5 minutes
+    const interval = setInterval(updateCount, 10000); // Every 10 seconds
 
     return () => clearInterval(interval);
   }, []);
