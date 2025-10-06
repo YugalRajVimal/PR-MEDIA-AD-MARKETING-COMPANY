4711ffefa6001a2778531e7107edec078fafc186
Yugal Raj Vimal <yugalrajvimalofficial@gmail.com>
2025-10-06 22:26:28 +0530

    --

--- src/Components/LandingPageComponents/Card3.js
+++ src/Components/LandingPageComponents/Card3.js
@@ -8,7 +8,7 @@ const Card3 = () => {
     <div className="w-full p-10 pt-10 flex justify-center items-center">
       <div className="relative h-28 max-w-[500px] md:max-w-[350px] text-black flex justify-center items-center font-bold w-full border border-zinc-700 bg-[#dbc3ab] md:bg-[#f7dbb6] border-[2px]  rounded-t-xl rounded-l-xl p-4  md:p-6 mx-auto">
         <div className="flex flex-col gap-4 text-center">
-          <p>Schedule your free audit call today.</p>
+          <p>Speak to our team today.</p>
         </div>
         {/* <div class="w-0 h-0  border-l-[20px] border-l-transparent border-r-[20px] border-r-transparent border-b-[40px] border-b-transparent border-b-2 border-black"></div> */}
         <div class="absolute -right-[13px] -bottom-3 w-0 h-0 border-l-[19px] rotate-[27deg] border-l-transparent border-r-[20px] border-r-transparent border-b-[40px] border-b-[#3f3f46] z-[-1]"></div>
